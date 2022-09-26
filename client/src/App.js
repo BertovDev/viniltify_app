ba95c8dcec764dc0472fb9ffee8e0d90b86cab6d
Bautista Berto <bertobautista03@gmail.com>
2022-09-26 15:23:43 -0300

    Model implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React,{Suspense} from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Login from "./components/Login.js"
+import Login from "./components/Login.js";
 import Dashboard from "./components/Dashboard.js";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
-const code = new URLSearchParams(window.location.search).get("code")
+import { Model } from "./modelCode/Vinyl.js";
+import { Model2 } from "./modelCode/Vinyl2.js";
+import { Model3 } from "./modelCode/Vinyl_3.js";
+
+const code = new URLSearchParams(window.location.search).get("code");
 
 function App() {
   return (
     code ? <Dashboard code={code}/> : <Login/>
-  )
+    // <Canvas style={{height:"100vh"}}>
+    //   {/* <ambientLight/> */}
+    //   <OrbitControls />
+    //   <Suspense fallback={null}>
+    //     {/* <Model/> */}
+    //     <Model2 />
+    //   </Suspense>
+    // </Canvas>
+  );
 }
 
 export default App;
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,96 +1,126 @@
-import React, { useState, useEffect } from 'react'
-import { Container, Form, Modal, Button } from 'react-bootstrap'
-import SpotifyWebApi from 'spotify-web-api-node'
+import React, { useState, useEffect, Suspense } from "react";
+import { Container, Form, Modal, Button } from "react-bootstrap";
+import SpotifyWebApi from "spotify-web-api-node";
 
-import useAuth from './useAuth'
-import TrackSearchResult from './TrackSearchResult'
-import Player from './Player'
+import useAuth from "./useAuth";
+import TrackSearchResult from "./TrackSearchResult";
+import Player from "./Player";
+
+import { Canvas } from "@react-three/fiber";
+import { Loader, OrbitControls } from "@react-three/drei";
+import { Model } from "../modelCode/Vinyl";
+import { Model2 } from "../modelCode/Vinyl2";
 
 const spotifyApi = new SpotifyWebApi({
-    clientId: "c9833a3d046f479f9d742874913f4428",
-
-})
+  clientId: "c9833a3d046f479f9d742874913f4428",
+});
 
 export default function Dashboard({ code }) {
-    const accessToken = useAuth(code)
+  const accessToken = useAuth(code);
 
-    const [search, setSearch] = useState("")
-    const [searchResults, setSearchResults] = useState([])
-    const [showModal, setShowModal] = useState(false)
-    const [playingTrack, setPlayingTrack] = useState()
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [playingTrack, setPlayingTrack] = useState();
 
-    function chooseTrack(track) {
-        setPlayingTrack(track)
-        setSearch("")
-    }
+  function chooseTrack(track) {
+    setPlayingTrack(track);
+    setSearch("");
+  }
 
-    const handleClose = () => setShowModal(false)
-    const handleOpen = () => setShowModal(true)
+  const handleClose = () => setShowModal(false);
+  const handleOpen = () => setShowModal(true);
 
-    useEffect(() => {
-        if (!accessToken) return
-        spotifyApi.setAccessToken(accessToken)
-    }, [accessToken])
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
 
-    useEffect(() => {
-        if (!search) return setSearchResults([])
-        if (!accessToken) return
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
 
-        let cancel = false
+    let cancel = false;
 
-        spotifyApi.searchTracks(search).then(res => {
-            if (cancel) return
-            setSearchResults(res.body.tracks.items.map(track => {
-                const smallestAlbumImage = track.album.images.reduce(
-                    (smallest, image) => {
-                        if (image.height < smallest.height) return image;
-                        return smallest
-                    }, track.album.images[0])
+    spotifyApi.searchTracks(search).then((res) => {
+      if (cancel) return;
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image;
+              return smallest;
+            },
+            track.album.images[0]
+          );
 
-                return {
-                    artist: track.artists[0].name,
-                    title: track.name,
-                    uri: track.uri,
-                    albumUrl: smallestAlbumImage.url
-                }
-            }))
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumImage.url,
+          };
         })
+      );
+    });
 
-        return () => cancel = true
-    }, [search, accessToken])
+    return () => (cancel = true);
+  }, [search, accessToken]);
 
-    return (
-        <Container className='d-flex flex-column py-2' style={{ height: "100vh" }}>
+  return (
+    <>
+      <Container
+        className="d-flex flex-column py-2"
+        style={{ width: "100%",background:"black" }}
+      >
+        <Button variant="primary" onClick={handleOpen}>
+          Search Songs/Artists
+        </Button>
 
-            <Button variant="primary" onClick={handleOpen}>
-                Search Songs/Artists
+        <Modal
+          show={showModal}
+          onHide={handleClose}
+          size="lg"
+          style={{ overflowY: "auto" }}
+        >
+          <Modal.Header>
+            <Form.Control
+              type="search"
+              placeholder="Search Songs/Artists"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            ></Form.Control>
+          </Modal.Header>
+
+          <Modal.Body s>
+            {searchResults.map((track) => (
+              <TrackSearchResult
+                track={track}
+                key={track.uri}
+                chooseTrack={chooseTrack}
+                closeModal={handleClose}
+              />
+            ))}
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button variant="danger" onClick={handleClose}>
+              Close
             </Button>
+          </Modal.Footer>
+        </Modal>
 
-            <Modal show={showModal} onHide={handleClose} size="lg" style={{ overflowY: "auto" }}>
-                <Modal.Header>
-                    <Form.Control type="search"
-                        placeholder='Search Songs/Artists'
-                        value={search}
-                        onChange={(e) => setSearch(e.target.value)}>
-                    </Form.Control>
-                </Modal.Header>
-
-                <Modal.Body s>
-                    {searchResults.map(track => (
-                        <TrackSearchResult track={track}
-                            key={track.uri}
-                            chooseTrack={chooseTrack}
-                            closeModal={handleClose}
-                        />
-                    ))}
-                </Modal.Body>
-
-                <Modal.Footer>
-                    <Button variant="danger" onClick={handleClose}>Close</Button>
-                </Modal.Footer>
-            </Modal>
-
-            <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
-        </Container>
-    )
+        <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+      </Container>
+      {/* ThreeJs code  */}
+      <Canvas style={{height:"100vh",background:"black"}}>
+        {/* <ambientLight/> */}
+        <OrbitControls />
+        <Suspense fallback={null}>
+          {/* <Model/> */}
+          <Model2 />
+        </Suspense>
+      </Canvas>
+    </>
+  );
 }
