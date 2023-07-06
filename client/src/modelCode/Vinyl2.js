1910194b0507bccac9f5908af6af73cd8b7103e3
BertovDev <bertobautista18@gmail.com>
2023-07-06 00:17:07 -0300

    Add simple particle movement

--- client/src/modelCode/Vinyl2.js
+++ client/src/modelCode/Vinyl2.js
@@ -1,13 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import {
-  useGLTF,
-  PerspectiveCamera,
-  SpotLight,
-  OrbitControls,
-  PointMaterial,
-  Point,
-  Points,
-} from "@react-three/drei";
+import { useGLTF, PerspectiveCamera, SpotLight, OrbitControls, PointMaterial, Point, Points } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { TWEEN } from "three/examples/jsm/libs/tween.module.min.js";
 import * as THREE from "three";
@@ -28,13 +20,7 @@ const listener = new THREE.AudioListener();
 const sound = new THREE.Audio(listener);
 const audioLoader = new THREE.AudioLoader();
 
-export function Model2({
-  vinylPlay,
-  setVinylPlay,
-  setCurrentPlaying,
-  setCurrentState,
-  props,
-}) {
+export function Model2({ vinylPlay, setVinylPlay, setCurrentPlaying, setCurrentState, props }) {
   const { nodes, materials } = useGLTF("/vinyl2.glb");
   const animationSpeed = 0.04;
   const [clicked, setClicked] = useState(false);
@@ -52,8 +38,11 @@ export function Model2({
   const refDiskSupport = useRef();
   const refControls = useRef();
   const refParticle = useRef();
+  const refParticle2 = useRef();
+  const refParticle3 = useRef();
 
   let num;
+  let positionAux = 0.01;
 
   const { camera, scene } = useThree();
   camera.add(listener);
@@ -120,7 +109,10 @@ export function Model2({
 
       if (clicked) {
         refLight.current.rotation.y += 0.02;
-        console.log(refParticle.current.position.y);
+        refParticle.current.position.y += positionAux * (delta * 20);
+        refParticle2.current.position.y += positionAux * (delta * 20);
+        refParticle3.current.position.y += positionAux * (delta * 20);
+        if (refParticle3.current.position.y >= 1.5 || refParticle.current.position.y <= 0) positionAux *= -1;
       }
     },
     [clicked]
@@ -132,62 +124,23 @@ export function Model2({
 
   return (
     <group {...props} dispose={null}>
-      <OrbitControls
-        ref={refControls}
-        enablePan={false}
-        maxDistance={8}
-        maxPolarAngle={1.5}
-        zoomSpeed={2}
-      />
+      <OrbitControls ref={refControls} enablePan={false} maxDistance={8} maxPolarAngle={1.5} zoomSpeed={2} />
       <group ref={refDisk} position={[0, 0, 0]}>
         <mesh geometry={nodes.Mesh_1.geometry} material={materials.Record} />
         <mesh geometry={nodes.Mesh_2.geometry} material={materials.Label} />
-        <mesh
-          geometry={nodes.Text021.geometry}
-          material={materials.Record}
-          position={[0, 0.01, -0.1]}
-          scale={0.54}
-        />
-        <mesh
-          geometry={nodes.Text024.geometry}
-          material={materials.Record}
-          position={[-0.2, 0.01, 0.1]}
-        />
+        <mesh geometry={nodes.Text021.geometry} material={materials.Record} position={[0, 0.01, -0.1]} scale={0.54} />
+        <mesh geometry={nodes.Text024.geometry} material={materials.Record} position={[-0.2, 0.01, 0.1]} />
       </group>
 
       <group ref={refDiskSupport} position={[0, -0.04, 0]}>
-        <mesh
-          geometry={nodes.Cylinder002.geometry}
-          material={materials.TurntableRubber}
-        />
-        <mesh
-          geometry={nodes.Cylinder002_1.geometry}
-          material={materials.TurnTableMetal}
-        />
+        <mesh geometry={nodes.Cylinder002.geometry} material={materials.TurntableRubber} />
+        <mesh geometry={nodes.Cylinder002_1.geometry} material={materials.TurnTableMetal} />
       </group>
       {/* <pointLight intensity={300} decay={2} position={[-3.55, 1.1, -2.94]} rotation={[-1.84, 0.6, 1.93]} /> */}
-      <PerspectiveCamera
-        makeDefault={false}
-        far={100}
-        near={0.1}
-        fov={31.08}
-        position={[-2.97, 5.39, 5.48]}
-        rotation={[-0.8, -0.4, -0.38]}
-      />
-      <mesh
-        geometry={nodes.Spindle.geometry}
-        material={materials.SpindleMetal}
-      />
-      <mesh
-        geometry={nodes.Record_Player_Body.geometry}
-        material={materials["Record Player"]}
-        position={[0.22, -0.36, 0.17]}
-      />
-      <mesh
-        geometry={nodes.VolumeKnob.geometry}
-        material={materials.VolumeKnob}
-        position={[1.39, -0.2, 0.7]}
-      />
+      <PerspectiveCamera makeDefault={false} far={100} near={0.1} fov={31.08} position={[-2.97, 5.39, 5.48]} rotation={[-0.8, -0.4, -0.38]} />
+      <mesh geometry={nodes.Spindle.geometry} material={materials.SpindleMetal} />
+      <mesh geometry={nodes.Record_Player_Body.geometry} material={materials["Record Player"]} position={[0.22, -0.36, 0.17]} />
+      <mesh geometry={nodes.VolumeKnob.geometry} material={materials.VolumeKnob} position={[1.39, -0.2, 0.7]} />
       <mesh
         ref={ref}
         onPointerOver={() => {
@@ -207,28 +160,12 @@ export function Model2({
         rotation={[-Math.PI, 0, 0]}
       >
         <group position={[0, -0.07, -0.26]} rotation={[1.49, 0, 0]}>
-          <mesh
-            geometry={nodes.Cylinder004.geometry}
-            material={materials.VolumeKnob}
-          />
-          <mesh
-            geometry={nodes.Cylinder004_1.geometry}
-            material={materials.TurnTableMetal}
-          />
+          <mesh geometry={nodes.Cylinder004.geometry} material={materials.VolumeKnob} />
+          <mesh geometry={nodes.Cylinder004_1.geometry} material={materials.TurnTableMetal} />
         </group>
       </mesh>
-      <mesh
-        geometry={nodes.SPINNYBOI_4000.geometry}
-        material={materials.LogoText}
-        position={[-1.25, -0.15, 1.47]}
-      />
-      <mesh
-        receiveShadow
-        geometry={nodes.Plane.geometry}
-        material={materials.tABLE}
-        scale={3}
-        position={[0, -0.39, 0]}
-      />
+      <mesh geometry={nodes.SPINNYBOI_4000.geometry} material={materials.LogoText} position={[-1.25, -0.15, 1.47]} />
+      <mesh receiveShadow geometry={nodes.Plane.geometry} material={materials.tABLE} scale={3} position={[0, -0.39, 0]} />
 
       {diskArray.map((el) => {
         return (
@@ -255,14 +192,10 @@ export function Model2({
         );
       })}
       <Points>
-        <PointMaterial
-          vertexColors
-          size={35}
-          sizeAttenuation={false}
-          depthWrite={true}
-          toneMapped={false}
-        >
+        <PointMaterial vertexColors size={35} sizeAttenuation={false} depthWrite={true} toneMapped={false}>
           <Point ref={refParticle} color={"orange"} position={[-1, 0, -1]} />
+          <Point ref={refParticle2} color={"orange"} position={[-1, 0.5, -1]} />
+          <Point ref={refParticle3} color={"orange"} position={[-1, 1, -1]} />
         </PointMaterial>
       </Points>
       <Lights refLight={refLight} />
