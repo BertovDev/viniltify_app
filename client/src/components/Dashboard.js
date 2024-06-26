8a279bf3cf996154c36aa8404ba353fcbb252405
BertovDev <bertobautista18@gmail.com>
2024-06-26 15:45:06 -0300

    Remove comments

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -7,7 +7,7 @@ import {
   Alert,
   ProgressBar,
   Col,
-  Row
+  Row,
 } from "react-bootstrap";
 
 import TrackSearchResult from "./TrackSearchResult";
@@ -18,7 +18,13 @@ import { Model2 } from "../modelCode/Vinyl2";
 import DiskPlane from "./DiskPlane";
 
 // Icons
-import { Coin, Github, Linkedin, Twitter, WindowFullscreen } from "react-bootstrap-icons";
+import {
+  Coin,
+  Github,
+  Linkedin,
+  Twitter,
+  WindowFullscreen,
+} from "react-bootstrap-icons";
 import { useControls } from "leva";
 import { TWEEN } from "three/examples/jsm/libs/tween.module.min";
 
@@ -37,13 +43,22 @@ export default function Dashboard() {
   const [currentPlaying, setCurrentPlaying] = useState("");
   const [currentState, setCurrentState] = useState();
 
-  
-  window.mobileCheck = function() {
+  window.mobileCheck = function () {
     let check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    (function (a) {
+      if (
+        /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+          a
+        ) ||
+        /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+          a.substr(0, 4)
+        )
+      )
+        check = true;
+    })(navigator.userAgent || navigator.vendor || window.opera);
     return check;
   };
-  let loaderFontSize = window.mobileCheck() ? "22px" : "30px"
+  let loaderFontSize = window.mobileCheck() ? "22px" : "30px";
 
   const refDiskInfo = useRef();
 
@@ -148,107 +163,12 @@ export default function Dashboard() {
     <>
       <Container
         className="d-flex flex-row gap-2 justify-content-between py-2"
-        style={{ width: "100%", cursor: "auto"}} >
-        {/* <Button
-          variant="success"
-          style={{ width: "20%" }}
-          onClick={handleOpenHelp}
-        >
-          How to use
-        </Button> */}
-
-        {/* <Button
-          variant="primary"
-          style={{ width: "100%" }}
-          onClick={handleOpen}
-        >
-          Search Songs/Artists
-        </Button> */}
-        {/* <Alert
-          style={{
-            height: "100%",
-            marginBottom: 0,
-          }}
-          variant="dark"
-          className="container fs-4 text-dark d-flex justify-content-center"
-        >
-          <div
-            style={{
-              color: "#0f1d5a",
-            }}
-          >
-            Song selected:{" "}
-            <span style={{ paddingLeft: "10px" }}> {currentPlaying}</span>
-          </div>
-        </Alert> */}
-
-        {/* <Modal
-          show={showModal}
-          onHide={handleClose}
-          size="lg"
-          style={{ overflowY: "auto" }}
-        >
-          <Modal.Header>
-            <Form.Control
-              type="search"
-              placeholder="Search Songs/Artists"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            ></Form.Control>
-          </Modal.Header>
-
-          <Modal.Body s>
-            {searchResults.map((track) => (
-              <TrackSearchResult
-                track={track}
-                key={track.uri}
-                chooseTrack={chooseTrack}
-                closeModal={handleClose}
-              />
-            ))}
-          </Modal.Body>
-
-          <Modal.Footer>
-            <Button variant="danger" onClick={handleClose}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal> */}
-
-        {/* <Modal
-          show={showHelpModal}
-          onHide={handleCloseHelp}
-          size="lg"
-          aria-labelledby="contained-modal-title-vcenter"
-          style={{ cursor: "auto" }}
-          centered
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter">
-              How to use
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <h4>Move the scene</h4>
-            <p>
-              Maintain left click to move the scene or use the mouse wheel to
-              zoom!
-            </p>
-            <hr />
-            <h4>Select a song</h4>
-            <p>
-              Touch/click the vinyls to select the song and then touch/click the
-              turtable lever to play it!
-            </p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={handleCloseHelp}>Close</Button>
-          </Modal.Footer>
-        </Modal> */}
-
-        {/* <Player accessToken={accessToken} trackUri={playingTrack?.uri} vinilPlay={vinylPlay} setVinilPlay={setVinylPlay}/> */}
+        style={{ width: "100%", cursor: "auto" }}
+      >
         <Row
-          xs={1} sm={1} md={2}
+          xs={1}
+          sm={1}
+          md={2}
           style={{
             display: "flex",
             justifyContent: "space-between",
@@ -256,35 +176,30 @@ export default function Dashboard() {
             width: "100%",
           }}
         >
-          <Col style={{padding:"5px 12px", color: "white", fontSize: "16px", fontFamily: "monospace" }}>
-           Playing: <span style={{color:"#fab14e",fontSize:"14px"}}>{currentPlaying}</span>
+          <Col
+            style={{
+              padding: "5px 12px",
+              color: "white",
+              fontSize: "16px",
+              fontFamily: "monospace",
+            }}
+          >
+            Playing:{" "}
+            <span style={{ color: "#fab14e", fontSize: "14px" }}>
+              {currentPlaying}
+            </span>
           </Col>
-          <Col style={{padding:"5px 12px", color: "gray", fontSize: "15px", fontFamily: "monospace" }}>
-          ☝️🤓 Click/touch a vinyl and then the turntable lever to play the song 
+          <Col
+            style={{
+              padding: "5px 12px",
+              color: "gray",
+              fontSize: "15px",
+              fontFamily: "monospace",
+            }}
+          >
+            ☝️🤓 Click/touch a vinyl and then the turntable lever to play the
+            song
           </Col>
-          {/* <Col style={{ width:"auto",paddingTop:"10px"}}>
-          <a
-            style={{ padding: "0px 10px" }}
-            href="https://github.com/BertovDev"
-            target="_blank"
-          >
-            <Github color="white" size={window.mobileCheck() ? 20 : 25} />
-          </a>
-          <a
-            style={{ padding: "0 10px" }}
-            href="https://www.linkedin.com/in/bautista-berto/"
-            target="_blank"
-          >
-            <Linkedin color="white" size={window.mobileCheck() ? 20 : 25} />
-          </a>
-          <a
-            style={{ padding: "0 10px" }}
-            href="https://twitter.com/tongenjs"
-            target="_blank"
-          >
-            <Twitter color="white" size={window.mobileCheck() ? 20 : 25} />
-          </a>
-          </Col> */}
         </Row>
       </Container>
       {/* ThreeJs code  */}
@@ -344,7 +259,13 @@ export default function Dashboard() {
           ☝️🤓 A compilation of some of my favorite songs...
           You can move the screen by pressing it down and move
         `} // Text
-        innerStyles={{ fontSize: loaderFontSize,display:"flex",justifyContent: "center",alignItems:"center",width:"100%"}}
+        innerStyles={{
+          fontSize: loaderFontSize,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+        }}
       />
     </>
   );
